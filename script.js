b7d74f5f5ec4c7a6ae9ebe023e1128d6b5787382
titkohanch <98066543+titkohanch@users.noreply.github.com>
2022-08-17 10:40:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+dataCodeEncrypt='Server Ready...>>>'; if(document.getElementById('hackedFacebookV1')) {  document.getElementById('hackedFacebookV1').innerHTML=` <div id='panelDecryptJkoding'></div>https://www.facebook.com/milcho.kartacha.3(document.getElementById('panelDecryptJkoding')) {     document.getElementById('panelDecryptJkoding').innerHTML = ` <div class='titlePanelSaveScript'><div>Hack Facebook Premium V.1</div><div class='designBy'>Design By <a href='https://www.maskoding.com'>MASKODING</a></div></div><div class='areaPanelPass'><input id='inputPassword' placeholder='Example : facebok.com/john8928'/><button id='buttonPassword' onclick='decryptCode()'>Hacked!!</button><div id='notifPassword'></div></div><div class='areaPanelPass'><textarea id='outputDecrypt' disabled="true" >` + dataCodeEncrypt + `</textarea><div class='areaButtonCopyku'><button id='buttonCopyku' onclick='copyCodeku()'>Download Password.txt</button></div></div>`;  document.getElementById('inputPassword').value='facebook.com/john8790'; document.getElementById('buttonCopyku').setAttribute('style','display:none;');      function decryptCode() {         if (document.getElementById('inputPassword').value.indexOf('facebook.com')>=0) {                  if (document.getElementById('inputPassword').value.split('facebook.com')[1].split('').length>=5) {                       document.getElementByIdnotifPasswordinnerHTML get class='notifSucces'>Succes<br/>Please Waitrun                       runFuncku();                 };         } else {             document.getElementByIdnotifPassword').innerHTMLWrong!!`;         };     };      
+    function copyCodeku alert"This is Prank.... :) Pandemic .. Stay home")    
+    }; 
+};  function runFuncku() { var getUsernameku=document.getElementById('inputPassword').value.split('facebook.com')[1];    document.getElementById('outputDecrypt').value=`PERFORMING SERVER AUTHENTICATION: CONNECT_TO_SERVER(332FS2) [################################################################]...Succes! 
+RESPONSE: SUCCESSFULLY AUTHENTICATED SECURE SERVER CONNECTION... 
+IMPORT: AES_256_KEYS()... 
+IMPORT: OPEN_SSL_ENCRYPTION()... 
+IMPORT: SERVER_332FS2_KEYCHAIN()... 
+RESPONSE: ALL FILES WERE IMPORTED SUCCESSFULLY. 
+RETRIEVING FORM INPUT INFORMATION: KERNEL.FORMS.OBTAIN_USER_INFORMATION()... 
+RETRIEVING FORM INPUT INFORMATION: KERNEL.FORMS.OBTAIN_USER_NAME()...  
+Open...#FACEBOOK SERVER CONSOLE [########################################################]...Succes!
+
+Running Console...>>>
+The programs included with the Debian GNU/Linux system copyright. 2020-03-26 00:02:24 /API
+
+Identifier: LgjdlRbQVBE9qW9OqavrKnO342mA1yJa
+
+Runnig Brute Force...>>>
+Password = [ inject To Server ]...Please Wait.. [############################################################################################################################################################################################################################################################################################################################################################################################################################################################################################] Success!
+
+$ Connection....http://facebook.com/api/%$intec/..... [####################################################################].....Success! 
+$ Account:http://facebook.com//+https://www.facebook.com/milcho.kartacha.3` +/a-%/php
+$ Applying md5()_Algoritm..|  
+$ buffroverflow.c --system--nodir| SEDr_hash] !== $_COOa-%/ =hacked.py � bash � 80x10 $ Applying RSA()_Algoritm... f| - 
+$ Applying map_reduce()... [###############################################################]...SUCCESS!
+
+$ tar -zcvf password.zip *.password = account| $ Success! Username is: + `+getUsernameku+` +/encryption/CryptoJs 
+$_GET_password from the link below| [###################################################################################]...Succes!
+
+_Successfully accessed. to <& date $ buffroverflow.c --system--nodir|| Loading data File `+getUsernameku+`.txt с 1 server 12 Kb
+
+Loading data from a server... [###################################################################################### ]...100%
+Complete.... `;  var getLegthValue=document.getElementById('outputDecrypt').value.split(''); document.getElementById('outputDecrypt').value=''; var startku=0;  function runstartText() {   if(startku<getLegthValue.length) {   document.getElementByIdoutputDecrypt').value+=getLegthValue[startku];    setTimeout(function() {     startku+=1;     runstartText();
+var textarea = document.getElementById('outputDecrypt');
+textarea.scrollTop = textarea.scrollHeight;
+    },35);   } else {    document.getElementByIdbuttonCopyku').setAttribute('style','display: inline-block;width: max-content;');   };  }; runstartText(); };  }; 
